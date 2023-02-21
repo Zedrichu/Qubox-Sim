730b849066cf8934c70b71d4baceb7f545326803
Adrian Zvizdenco <adrzvizdencojr@gmail.com>
2023-02-21 02:21:04 +0100

    Created Solution & Declared Lexer Solution formed of 2 projects in F# and C#. The LEXER contains all the quantum grammar tokens required in parser productions.

---
+++ QuantumLanguage/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace QuantumLanguage
+
+module Parser =
+
+open System
+open QuantumAST
+
+let ParseQuCode code =
+    let lexbuf = Lexing.LexBuffer<_>.FromString code
+    let output = QuantumParser.start QuantumLexer.tokenize lexbuf
+    output
+
+Console.ReadKey(true) |> ignore
