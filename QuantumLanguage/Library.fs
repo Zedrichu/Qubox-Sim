d4c3f9aab016923f4744efdb6f5203e7ef1edf06
Adrian Zvizdenco <adrzvizdencojr@gmail.com>
2023-02-21 14:03:06 +0100

    Declared Parser and built QuLang grammar handler utilities with FsLex and FsYacc

--- QuantumLanguage/Library.fs
+++ QuantumLanguage/Library.fs
@@ -1,4 +1,17 @@
-﻿namespace QuantumLanguage
+﻿/* F#
+ -*- coding: utf-8 -*-
+Quantum Language Handler
+
+Description: Library to assist the parsing/compilation process of QuLang from user-input
+
+@__Author --> Created by Adrian Zvizdenco aka Zedrichu
+@__Date & Time --> Created on 21/02/2023
+@__Email --> adrzvizdencojr@gmail.com
+@__Version --> 1.2
+@__Status --> DEV
+*/
+
+namespace QuantumLanguage
 
 module Parser =
 
